d2c4a1c7a9737151b7c72201e6984de0838713a3
bugiiiii11 <bugiiiii@protonmail.com>
2025-01-09 03:24:17 +0200

    padding

--- client/src/components/DetailsPage/DetailsPage.jsx
+++ client/src/components/DetailsPage/DetailsPage.jsx
@@ -82,7 +82,7 @@ const DetailsPage = ({ isOpen, meme }) => {
     position: 'fixed',
     left: 0,
     right: 0,
-    top: '90px', // Adjusted to start right below the top bar
+    top: '200px',
     bottom: '60px',
     background: '#0A0B0F',
     borderTop: '1px solid rgba(255, 255, 255, 0.05)',
