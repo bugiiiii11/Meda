b58099efdc04dd1d672e2d23c6c1b9fe6d5164b7
bugiiiii11 <bugiiiii@protonmail.com>
2025-01-20 18:13:47 +0200

    QR code, loading pic

--- client/src/components/DesktopRestriction/DesktopRestriction.jsx
+++ client/src/components/DesktopRestriction/DesktopRestriction.jsx
@@ -8,32 +8,26 @@ const DesktopRestriction = () => {
         <div className="absolute inset-0 bg-gradient-to-r from-[#4B7BF5]/20 to-[#8A2BE2]/20 rounded-xl blur-xl" />
         
         {/* Content container */}
-        <div className="relative bg-gradient-to-r from-[#2A1B3D] to-[#1A1B2E] border border-[#4B7BF5]/20 
-          rounded-xl p-6 text-center">
-          
-          {/* Mobile Phone Icon */}
+        <div className="relative bg-gradient-to-r from-[#2A1B3D] to-[#1A1B2E] border border-[#4B7BF5]/20 rounded-xl p-6 text-center">
+          {/* QR Code Image */}
           <div className="flex justify-center mb-6">
-            <div className="w-16 h-16 relative">
-              <div className="absolute inset-0 border-4 border-[#4B7BF5] rounded-xl"></div>
-              <div className="absolute top-1 left-1/2 -translate-x-1/2 w-4 h-1 bg-[#4B7BF5] rounded-full"></div>
-              <div className="absolute bottom-1 left-1/2 -translate-x-1/2 w-3 h-3 border-2 border-[#4B7BF5] rounded-full"></div>
-            </div>
+            <img 
+              src="/assets/images/desktopQR.png" 
+              alt="Telegram QR Code"
+              className="w-48 h-48 rounded-lg"
+            />
           </div>
           
           {/* Title */}
           <h2 className="font-game-title text-2xl text-white mb-4">
-            Mobile Only App
+            Continue on Mobile
           </h2>
           
           {/* Description */}
           <p className="font-game-mono text-gray-400 mb-6">
-            This app is designed for mobile devices only. Please open this link on your mobile device.
+            Scan this QR code with your mobile device to continue using the app.
           </p>
-          
-          {/* Additional info */}
-          <p className="font-game-mono text-sm text-gray-500">
-            For the best experience, please use your mobile phone.
-          </p>
+
         </div>
       </div>
     </div>
